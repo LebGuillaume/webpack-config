e5f46d5103ca8d4629dd6eaf0bab1d816cb5094b
Guillaume LEBEGUE <lebegue.guillaume26@gmail.com>
2021-04-25 15:59:23 +0200

    Image webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import "../style/index.scss";
 import React from "react";
 import Recipes from "./Recipes";
+import sword from "../images/57297.jpg";
+import img from "../images/007_DKM_01.jpg";
 const App = () => {
   return (
     <>
@@ -9,8 +11,10 @@ const App = () => {
         <section>
           <h1>yo lallalal</h1>
         </section>
+        <img src={sword} alt="sword" width="250" />
+        <img src={img} alt="img" width="300" />
+        <Recipes></Recipes>
       </main>
-      <Recipes></Recipes>
     </>
   );
 };
